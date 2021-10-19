2e44ff1d8c3963e47ab9d5f8612e4a4bcb109d91
AreSZerA <784795211@qq.com>
2021-10-19 14:46:27 +0800

    Fixed a DEADLY problem.

--- browser.go
+++ browser.go
@@ -6,10 +6,12 @@ import (
 	"strconv"
 )
 
+// OpenInBrowser opens http://127.0.0.1:port in default browser
 func OpenInBrowser() {
 	OpenUrlInBrowser("http://127.0.0.1:" + strconv.Itoa(GetServerPort()))
 }
 
+// OpenUrlInBrowser opens URL in default browser
 func OpenUrlInBrowser(url string) {
 	var cmd string
 	switch runtime.GOOS {
@@ -21,5 +23,8 @@ func OpenUrlInBrowser(url string) {
 		PrintWarning("Failed to open browser, unknown OS: " + runtime.GOOS)
 		return
 	}
-	exec.Command(cmd, url)
+	err := exec.Command(cmd, url).Start()
+	if err != nil {
+		PrintError("Failed to open browser: " + err.Error())
+	}
 }
